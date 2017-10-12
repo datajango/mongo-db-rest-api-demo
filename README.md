4a540744ccda974d00eed198bfd45769191b5dbb
unknown <tony@leotta.com>
2017-10-12 16:15:28 -0400

    Step 4

--- README.md
+++ README.md
@@ -33,4 +33,30 @@ app.use(logger('dev));
 
 # Step 4: Add 404 Handler
 
+```
+
+// Catch 440 Errors and forward them to an error handler 
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handler function
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {};
+    const status = err.status || 500;
+
+    // Respond to client
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    });
+
+    // Respond to ourselves
+    console.error(err);
+});
+```
+
 
--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 require('dotenv').config();
 const express = require('express');
-const logger = require('morgan');
+const morgan = require('morgan');
 
 const app = express();
 
 // Middleware
-app.use(logger());
+app.use(morgan('combined'))
 
 // Routes
 
 // Catch 440 Errors and forward them to an error handler 
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 // Error handler function
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {};
+    const status = err.status || 500;
 
+    // Respond to client
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    });
+
+    // Respond to ourselves
+    console.error(err);
+});
 
 // Start tthe server
 const port = app.get('port') || process.env.PORT || 4200;
-//app.listen(port, ()=>console.log(`Server is listening on port $port(port).`));
+app.listen(port, ()=>console.log(`Server is listening on port port ${port}.`));
